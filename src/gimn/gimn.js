995383835c09689e1738d00c70df6b039edd19e7
nazi <toktonalykyzynazi3675478@gmail.com>
2024-02-19 15:47:57 +0600

    yhgtfd

--- src/gimn/gimn.js
+++ src/gimn/gimn.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
-
-import muz4 from "../music/гимн-кр.mp3";
+import muz4 from "../music/гимнкрмелодия21 мелодия.mp3";
 
 const Gimn = () => {
   const [isNationalAnthemPlaying, setNationalAnthemPlaying] = useState(false);
 
   useEffect(() => {
+    const playNationalAnthem = () => {
+      setNationalAnthemPlaying(true);
+      const audio = new Audio(muz4);
+      audio.play()
+        .then(() => {
+          console.log('Гимн воспроизведен');
+        })
+        .catch((error) => {
+          console.error('Ошибка воспроизведения: ', error);
+        })
+        .finally(() => {
+          setNationalAnthemPlaying(false);
+        });
+    };
+
     const currentDate = new Date();
     const targetTime = new Date(currentDate);
-    targetTime.setHours(8,26,0);
-
-
+    targetTime.setHours(15, 41, 0); 
     let timeUntilNextAnthem = targetTime - currentDate;
     if (timeUntilNextAnthem < 0) {
       timeUntilNextAnthem += 24 * 60 * 60 * 1000; 
@@ -25,34 +37,11 @@ const Gimn = () => {
     };
   }, []);
 
-  const playNationalAnthem = () => {
-  setNationalAnthemPlaying(true);
-  const audio = new Audio(muz4);
-  audio.play()
-    .then(() => {
-      console.log('Гимн воспроизведен');
-    })
-    .catch((error) => {
-      console.error('Ошибка воспроизведения: ', error);
-    })
-    .finally(() => {
-      setNationalAnthemPlaying(false);
-    });
-};
   return (
     <div>
       {isNationalAnthemPlaying ? (
-        <p></p>
-      ) : (
-        <p style={{
-          fontSize:"36px",
-          color:"white",
-          marginLeft:"1040px",
-          marginTop:"-550px",
-          position:"absolute",
-        
-        }}>...</p>
-      )}
+        <p>Гимн воспроизводится...</p>
+      ) : null}
     </div>
   );
 };
